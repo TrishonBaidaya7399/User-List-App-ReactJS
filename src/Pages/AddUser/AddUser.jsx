6b736609705427b60f4bc3821b9ea4e718a1dbeb
TrishonBaidaya7399 <githubtrishon@gmail.com>
2024-02-10 15:21:13 +0600

    updated

--- src/Pages/AddUser/AddUser.jsx
+++ src/Pages/AddUser/AddUser.jsx
@@ -124,235 +124,250 @@ const AddUser = () => {
       </div>
       <div>
         <form>
-          <div className="flex gap-6 flex-col md:flex-row">
+          <div className="flex gap-6 flex-col md:flex-row-reverse">
             <div className="form border-2 rounded-lg bg-white drop-shadow-xl p-6 w-full">
               <h2 className="text-xl font-bold text-blue-500 pb-1 mb-3 border-b-2 border-blue-500 w-fit px-2">
                 User Details
               </h2>
-              <div className="my-2 flex gap-2">
+              <div className="my-2">
                 <label>
                   First Name:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="firstName"
                     value={newUser.firstName}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
               <label>
                 Last Name:
+                </label>
                 <input
-                  className="input bg-gray-200 h-8 ml-2 rounded-md"
+                  className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                   type="text"
                   name="lastName"
                   value={newUser.lastName}
                   onChange={handleInputChange}
                 />
-              </label>
 
-              <div className="my-2"></div>
 
               <div className="my-2">
                 <label>
                   Email:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="email"
                     value={newUser.email}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
               <div className="my-2">
                 <label>
                   Avatar URL:
+                </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="image"
                     value={newUser.image}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
               <div className="my-2">
                 <label>
                   Age:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="number"
                     name="age"
                     value={newUser.age}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Gender:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="gender"
                     value={newUser.gender}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Phone:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="phone"
                     value={newUser.phone}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Domain:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="domain"
                     value={newUser.domain}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Blood Group:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="bloodGroup"
                     value={newUser.bloodGroup}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Birth Date:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="date"
                     name="birthDate"
                     value={newUser.birthDate}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Address:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="address"
                     placeholder="Street & Suite"
                     value={newUser.address.address}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   City:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="city"
                     placeholder="City"
                     value={newUser.address.city}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
             </div>
             <div className="form border-2 rounded-lg bg-white drop-shadow-xl p-6 w-full">
+            <h2 className="text-xl font-bold text-blue-500 pb-1 mb-3 border-b-2 border-blue-500 w-fit px-2">
+                Preview
+              </h2>
+            <div className="text-center p-4 rounded-lg bg-gray-100 drop-shadow-xl mt-3 mb-12">
+              <img
+                src={newUser.image ? newUser.image : "https://as2.ftcdn.net/v2/jpg/04/89/93/27/1000_F_489932758_qfhzj92KwhZ5Lr2APhi5FJNbnAkq9BIB.jpg"}
+                alt={newUser.firstName}
+                className="w-40 h-40 rounded-full mx-auto mb-4 border-2 border-blue-500 p-1"
+              />
+              <div className="mx-auto">
+                <p className="text-xl font-bold text-blue-600">{`${newUser?.firstName ? newUser?.firstName : "First Name" } ${newUser?.lastName ? newUser?.lastName : "Last Name"}`}</p>
+                <p className="text-gray-400">{`${newUser?.company?.title ? newUser?.company?.title : "Title"}`}</p>
+                <p className="text-gray-400 font-semibold text-lg">{`Company: ${newUser?.company?.name ? newUser?.company?.name : "Company Name"}`}</p>
+              </div>
+            </div>
               <h2 className="text-xl font-bold text-blue-500 pb-1 mb-3 border-b-2 border-blue-500 w-fit px-2">
                 Company Details
               </h2>
               <div className="my-2">
                 <label>
                   Company Name:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="name"
                     value={newUser.company.name}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Title:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="title"
                     value={newUser.company.title}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Department:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="department"
                     value={newUser.company.department}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
 
               <div className="my-2">
                 <label>
                   Address:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="companyAddress"
                     placeholder="Street & Suite"
                     value={newUser.company.address.address}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
               <div className="my-2">
                 <label>
                   City:
+                  </label>
                   <input
-                    className="input bg-gray-200 h-8 ml-2 rounded-md"
+                    className="input bg-gray-200 h-8 ml-2 rounded-md w-full"
                     type="text"
                     name="companyCity"
                     placeholder="City"
                     value={newUser.company.address.companyCity}
                     onChange={handleInputChange}
                   />
-                </label>
               </div>
+              
             </div>
           </div>
           <div className="flex justify-center py-6">
